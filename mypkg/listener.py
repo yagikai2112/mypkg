173c3ee045d261de86ee1600744b1d7e7266ac0c
Kaito Yagiuchi <Yagisan-k@outlook.jp>
2025-12-11 22:28:05 +0900

    クラス作成

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -3,15 +3,15 @@ from rclpy.node import Node
 from std_msgs.msg import Int16
 
 
-rclpy.init()
-node = Node("listener")
-
-
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+class Listener(Node):
+    def __init__(self):
+        super().__init__("listener")
+        self.pub = self.create_subscription(Int16, "countup", self.cb, 10)
+    def cb(self, msg):
+        self.get_logger().info("Listen: %d" % msg.data)
 
 
 def main():
-    pub = node.create_subscription(Int16, "countup", cb, 10)
+    rclpy.init()
+    node =Listener()
     rclpy.spin(node)
--- mypkg/talker.py
+++ mypkg/talker.py
@@ -2,20 +2,20 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
-
-
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n += 1
+class Talker(Node):
+    def __init__(self):
+        super().__init__("takler")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.create_timer(0.5, self.cb)
+        self.n = 0
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
 
 
 def main():
-    node.create_timer(0.5, cb)
+    rclpy.init()
+    node = Talker()
     rclpy.spin(node)
